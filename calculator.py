f411a2d886f7b60d185c89a61af22fb52696964a
N1NJA46 <109333741+N1NJA46@users.noreply.github.com>
2022-07-15 12:35:07 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+operation=int(input("1 for adittion 2 for subtraction 3 for mult 4 for division"))
+if opetration==1:
+	print(a+b)
+if opetration==2:
+	print(a-b)
+if opetration==3:
+	print(a*b)
+if opetration==4:
+	print(a/b)
